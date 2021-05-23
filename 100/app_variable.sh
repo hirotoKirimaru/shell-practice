9ef46c648752bf132085e9a5d612059807308b73
kirimaru <nainaistar@gmail.com>
2021-05-23 09:25:21 +0000

    [Add] 別にあるシェルを呼ぶ

---
+++ 100/app_variable.sh
@@ -0,0 +1,8 @@
+#/bin/sh
+
+another1="アナザー1"
+
+echo "(内部)別シェルの変数:"$another1
+# export another1=$another1
+# source another1=`$another1`
+export another1
--- 100/main.sh
+++ 100/main.sh
@@ -1,4 +1,10 @@
 #/bin/sh
 
 var1=あいうえお
-echo $var1
+echo "var1:"$var1
+echo "パラメータ1:"$0
+echo "パラメータ2:"$1
+
+source `dirname {0}`/app_variable.sh
+
+echo "別シェルの変数:"$another1
