be281894f0574e3e3316f1db6985082aa72aa8c2
kirimaru <nainaistar@gmail.com>
2021-05-28 07:03:40 +0000

    [Add] 処理追加

--- 200/test.sh
+++ 200/test.sh
@@ -3,3 +3,6 @@ echo -e "\e[31m赤色を指定"
 echo "色を指定しない"
 echo -e "\e[32m緑色を指定"
 echo -e "\e[33m$(cat result.txt)\e[m"
+result=************完了しました***************
+echo -e "\e[33m$result\e[m"
+echo -e "\e[33m$(echo $result)\e[m"
